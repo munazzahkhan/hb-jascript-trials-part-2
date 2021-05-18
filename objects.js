9941a6888e077a0ca9aa35396ad8fdf9dd8f2bae
Munazzah Hassan Khan <munazzah.hassan@gmail.com>
2021-05-17 18:05:41 -0700

    completed the objects.js file

--- objects.js
+++ objects.js
@@ -3,11 +3,34 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  const wordCounts = {};
+  // console.log(phrase.split(' '))
+  for (const word of phrase.split(' ')){
+    if (!phrase.word){ 
+      wordCounts[word] = 1;
+    } else {
+      wordCounts[word] += 1;
+    }
+  }
+  return wordCounts;
 }
 
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  const melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+  for (const p in melonPrices){
+    if (p == price){
+      console.log("here");
+      return (melonPrices[price]);
+    } 
+  }
+  return null;
 }
+
+console.log(getMelonsAtPrice(3.11))
