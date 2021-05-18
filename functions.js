971b9b8693d27ec2f7312e7c49b646fcddc2d9b8
Munazzah Hassan Khan <munazzah.hassan@gmail.com>
2021-05-17 17:15:38 -0700

    completed the functions.js file

--- functions.js
+++ functions.js
@@ -2,14 +2,27 @@
 
 
 // 1. isHometown
-const isHometown = town => town === 'San Francisco'
+const isHometown = town => town === 'San Francisco';
 
 
 // 2. getFullName
-
-// Define your function here
+const getFullName = (firstName, lastName) => `${firstName} ${lastName}`;
 
 
 // 3. calculateTotal
-
-// Define your function here
+const calculateTotal = (basePrice, state, tax = 0.05) => {
+    const subtotal = basePrice * (1 + tax);
+    let fee = 0;
+    if (state === 'CA'){
+        fee = 0.03 * subtotal;
+    }else if (state = 'PA'){
+        fee = 2;
+    }else if (state = 'MA'){
+        if (basePrice <= 100){
+            fee = 1;
+        } else {
+            fee = 3;
+        }
+    }
+    return (subtotal + fee);
+}
